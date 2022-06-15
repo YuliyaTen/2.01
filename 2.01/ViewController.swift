d3910bb74a4d79994abf3e24a4a91665edacd038
2510-day <juliia.ten@gmail.com>
2022-06-15 20:39:35 +0300

    Initial Commit

---
+++ 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.01
+//
+//  Created by Yuliya Ten on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
